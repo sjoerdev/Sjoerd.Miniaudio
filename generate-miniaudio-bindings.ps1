12ea386dfe6788fba889e24e9cd65cc9c4a6594f
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2026-05-22 19:00:24 +0200

    improve

--- generate-miniaudio-bindings.ps1
+++ generate-miniaudio-bindings.ps1
@@ -1,61 +1,48 @@
+# requires git, gcc, ClangSharpPInvokeGenerator
+
 $root = (Get-Location).ProviderPath
-$sourcePath = Join-Path $root "./miniaudio/"
-$outPath = Join-Path $root "./output/"
+$miniaudio_repo = Join-Path $root "./miniaudio/"
+$output_directory = Join-Path $root "./output/"
 
-function Assert-CommandExists($Name)
+# clone repo if it isnt there
+if (-not (Test-Path $miniaudio_repo))
 {
-    if (-not (Get-Command $Name -ErrorAction SilentlyContinue))
-    {
-        throw "Required command '$Name' was not found in PATH."
-    }
-}
-
-Assert-CommandExists 'git'
-Assert-CommandExists 'gcc'
-Assert-CommandExists 'ClangSharpPInvokeGenerator'
-
-if (-not (Test-Path $sourcePath))
-{
+    Write-Host "cloning miniaudio repository"
     & git clone "https://github.com/mackron/miniaudio.git"
 }
 
-if (Test-Path $outPath)
+# reset the output directory
+if (Test-Path $output_directory)
 {
-    Write-Host "cleaning output folder"
-    Remove-Item -Recurse -Force -Path $outPath
+    Write-Host "cleaning output directory"
+    Remove-Item -Recurse -Force -Path $output_directory
 }
+New-Item -ItemType Directory -Path $output_directory
 
-New-Item -ItemType Directory -Path $outPath
+# -c multi-file compatible-codegen generate-aggressive-inlining exclude-default-remappings generate-helper-types <# configuration for the generator#>
+# --file miniaudio.h <# file we want to generate bindings for #> 
+# --traverse miniaudio.h
+# -n Sjoerd.Miniaudio <# namespace of the bindings #>
+# --methodClassName Miniaudio <# class name where to put methods #>
+# --libraryPath miniaudio <# name of the DLL #>
+# -o ..\MiniaudioSharp\src <# output folder #>
 
-<#
-
-ClangSharpPInvokeGenerator `
-    -c multi-file compatible-codegen generate-aggressive-inlining exclude-default-remappings generate-helper-types <# configuration for the generator#> `
-    --file miniaudio\vendor\miniaudio.h <# file we want to generate bindings for #>  `
-    --traverse miniaudio\vendor\miniaudio.h `
-    -n MiniaudioSharp <# namespace of the bindings #> `
-    --methodClassName Miniaudio <# class name where to put methods #> `
-    --libraryPath miniaudio <# name of the DLL #> `
-    -o ..\MiniaudioSharp\src <# output folder #>
-
-#>
-
-$miniaudioC = Join-Path $sourcePath 'miniaudio.c'
-$miniaudioH = Join-Path $sourcePath 'miniaudio.h'
-$dllOut = Join-Path $outPath 'miniaudio.dll'
-$soOut = Join-Path $outPath 'libminiaudio.so'
-$bindingsOut = Join-Path $outPath 'MiniaudioBindings.cs'
+$miniaudio_c_path = Join-Path $miniaudio_repo "miniaudio.c"
+$miniaudio_h_path = Join-Path $miniaudio_repo "miniaudio.h"
+$miniaudio_dll_path = Join-Path $output_directory "miniaudio.dll"
+$miniaudio_so_path = Join-Path $output_directory "libminiaudio.so"
+$miniaudio_cs_path = Join-Path $output_directory "Bindings.cs"
 
 Write-Host "building dll"
-$winLibs = '-lwinmm', '-luser32', '-lgdi32', '-lole32', '-luuid'
-& gcc -shared -o $dllOut $miniaudioC @winLibs
+$mindows_links = '-lwinmm', '-luser32', '-lgdi32', '-lole32', '-luuid'
+& gcc -shared -o $miniaudio_dll_path $miniaudio_c_path @mindows_links
 
 Write-Host "building so"
-$linuxLibs = '-lpthread', '-lm'
-& gcc -shared -o $soOut $miniaudioC @linuxLibs
+$linux_links = '-lpthread', '-lm'
+& gcc -shared -o $miniaudio_so_path $miniaudio_c_path @linux_links
 
-Write-Host "generating bindings with clangsharp"
-& ClangSharpPInvokeGenerator -f $miniaudioH -I $sourcePath -o $bindingsOut -l miniaudio -n MiniaudioSharp -x c --additional '-DWIN32' --additional '-D_WINDOWS'
+Write-Host "generating bindings"
+& ClangSharpPInvokeGenerator -f $miniaudio_h_path -I $miniaudio_repo -o $miniaudio_cs_path -l miniaudio -n MiniaudioSharp -x c --additional '-DWIN32' --additional '-D_WINDOWS'
 
-Write-Host "Output directory: $outPath"
-Get-ChildItem -Path $outPath | Select-Object Name, Length | Format-Table -AutoSize
+Write-Host "bindings generated here: $output_directory"
+Get-ChildItem -Path $output_directory | Select-Object Name, Length | Format-Table -AutoSize
