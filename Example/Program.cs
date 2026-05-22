949412af948f626985fd8490c71b61c20857879c
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2026-05-22 23:27:47 +0200

    test file

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,8 +1,31 @@
 ﻿using System;
+using System.Text;
 using Sjoerd.Miniaudio;
 
-unsafe
+unsafe static class Program
 {
-    var version = ma.version_string();
-    Console.WriteLine(*version);
+    public static void Main()
+    {
+        ma_engine engine;
+
+        ma.engine_init(null, &engine);
+
+        ma_sound sound;
+
+        byte[] utf8 = Encoding.UTF8.GetBytes("test.wav\0");
+
+        fixed (byte* pFile = utf8)
+        {
+            ma.sound_init_from_file(&engine, (sbyte*)pFile, 0, null, null, &sound);
+            ma.sound_start(&sound);
+
+            Console.WriteLine("Playing...");
+            Console.ReadKey();
+
+            ma.sound_stop(&sound);
+            ma.sound_uninit(&sound);
+        }
+
+        ma.engine_uninit(&engine);
+    }
 }
